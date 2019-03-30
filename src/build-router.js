55a62351aba485f9f6bdd7573aa708811c067c0c
Chris <chris@uber5.com>
2019-03-30 10:15:57 +0200

    Added prettier and eslint, wip

---
+++ src/build-router.js
@@ -0,0 +1,14 @@
+function buildRouter({ express, store }) {
+  const router = express.Router();
+  router.get('/authorize', (req, res, next) => {
+    res.end('should authorize');
+    next();
+  });
+  router.get('/login', (req, res, next) => {
+    global.console.log('login... but we just call next...');
+    next();
+  });
+  return router;
+}
+
+module.exports = buildRouter;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-
-function buildRouter({ express, store }) {
-  const router = express.Router()
-  router.get('/authorize', (req, res, next) => {
-    res.end('should authorize')
-    next()
-  })
-  router.get('/login', (req, res, next) => {
-    console.log('login... but we just call next...')
-    next()
-  })
-  return router
-}
+const buildRouter = require('./build-router');
 
 module.exports = {
   buildRouter
-}
+};
