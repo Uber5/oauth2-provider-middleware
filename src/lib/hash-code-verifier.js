e3e0aa8cd338dd9e5f13f2181eccc81b0ac3bfe3
Siya Makhalima <siya@uber5.com>
2019-10-07 15:00:23 +0200

    removed the base64url encoder and fix the bug in the encoding function with Chris

--- src/lib/hash-code-verifier.js
+++ src/lib/hash-code-verifier.js
@@ -1,13 +1,15 @@
 const sha256 = require('js-sha256');
 
 module.exports = codeVerifier => {
-  let asciiCode = '';
-  codeVerifier.split('').forEach(char => {
-    asciiCode = `${asciiCode + char.charCodeAt(0)} `;
-  });
-  const hashed = sha256(asciiCode);
-
-  // eslint-disable-next-line no-buffer-constructor
-  const encoded = new Buffer(hashed, 'utf-8').toString('base64');
+  function base64URLEncode(str) {
+    return str
+      .toString('base64')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=/g, '');
+  }
+  const hashed = sha256(codeVerifier);
+  const bufferHash = Buffer.from(hashed, 'hex');
+  const encoded = base64URLEncode(bufferHash);
   return encoded;
 };
